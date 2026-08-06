f1b15bfa010f1d1d88a5b4de7ffc824bcc0c7be5
amir88976 <amirrostami77726@gmail.com>
2026-08-06 13:21:33 +0330

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,23 @@
+"""
+Personal AI System
+Core Package
+"""
+
+
+from core.brain import (
+    BrainEngine,
+    process_brain
+)
+
+
+from core.personality import (
+    PersonalityEngine
+)
+
+
+from core.decision import (
+    DecisionEngine
+)
+
+
+__version__ = "0.3"
